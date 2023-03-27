94cb4f34fce5b2c68f0757a38747d9ee3dce5561
Damien Redd <damiens.redd@gmail.com>
2023-03-27 13:13:35 -0500

    Cupcakes Starter Code

---
+++ starter/cupcakes.py
@@ -0,0 +1,18 @@
+class Cupcake:
+    def __init__ (self, name, price, flavor, frosting, filling):
+        self.name = name
+        self.price = price
+        self.flavor = flavor
+        self.frosting = frosting
+        self.filling = filling
+        self.sprinkles = []
+        
+    def add_sprinkles(self, *args):
+        for sprinkle in args:
+            self.sprinkles.append(sprinkle)
+        
+cupcake1 = Cupcake("Triple Chocolate", 2.99, "chocolate", "chocolate", "chocolate")
+
+cupcake1.add_sprinkles("Oreo crumbs", "chocolate", "vanilla")
+
+print(cupcake1.sprinkles)
