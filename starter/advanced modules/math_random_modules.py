52f0745a83d3328c7148d80b7c169b0ee8b3ae71
Damien Redd <damiens.redd@gmail.com>
2023-03-28 11:40:24 -0500

    Math and Random modules

---
+++ starter/advanced modules/math_random_modules.py
@@ -0,0 +1,31 @@
+import math 
+# print(help(math))
+
+value = 4.35
+# print(math.floor(value))
+# print(math.ceil(value))
+# print(round(value))
+
+# print(math.pi)
+
+import random
+# print(random.randint(1,10))
+
+# random.seed(101)
+# print(random.randint(0,100))
+
+# random.seed(101)
+# print(random.randint(0,100))
+# print(random.randint(0,100))
+# print(random.randint(0,100))
+# print(random.randint(0,100))
+# print(random.randint(0,100))
+
+mylist = list(range(0,20))
+# print(mylist)
+# print(random.choice(mylist))
+# print(random.choices(population=mylist, k=10))
+# print(random.sample(population=mylist,k=10))
+
+random.shuffle(mylist)
+# print(mylist)
