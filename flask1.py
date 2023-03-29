4257886d84cbddf8b42375a757494cc65e35f71f
Damien Redd <damiens.redd@gmail.com>
2023-03-29 08:58:35 -0500

    basic routing

--- flask1.py
+++ flask1.py
@@ -1,9 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/')  #127.0.0.1:5000 - landing page
 def index():
     return "<h1>Hello Puppy!</h1>"
+
+@app.route('/information') #127.0.0.1:5000/information - information page
+def info():
+    return "<h1>Puppies are cute!</h1>"
 
 if __name__ == "__main__":
     app.run()
