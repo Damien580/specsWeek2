76984a0b20833596abc653b3202e8f5c7fd2698f
Damien Redd <damiens.redd@gmail.com>
2023-03-30 10:30:48 -0500

    working code, pre-css

--- solution/py-proj-2-solution 2/cupcakes.py
+++ solution/py-proj-2-solution 2/cupcakes.py
@@ -1,110 +1,101 @@
-import csv
-from pprint import pprint
 from abc import ABC, abstractmethod
+from pprint import pprint
+import csv
 
+### Cupcake classes
 
-
-
-
-class Cupcake():
-    def __init__ (self, name, price, flavor, frosting, filling):
+class Cupcake(ABC):
+    size = "regular"
+    def __init__(self, name, price, flavor, frosting, filling):
         self.name = name
         self.price = price
         self.flavor = flavor
         self.frosting = frosting
         self.filling = filling
         self.sprinkles = []
-        
+
     def add_sprinkles(self, *args):
         for sprinkle in args:
             self.sprinkles.append(sprinkle)
-    
-         
+
+    @abstractmethod
+    def calculate_price(self, quantity):
+        return quantity * self.price
+
+
+class Regular(Cupcake):
+    size = "regular"
+
     def calculate_price(self, quantity):
         return quantity * self.price
 
 class Mini(Cupcake):
-    size = "Mini"
-    
+    size = "mini"
+
     def __init__(self, name, price, flavor, frosting):
         self.name = name
         self.price = price
         self.flavor = flavor
         self.frosting = frosting
         self.sprinkles = []
-        
-class Regular(Cupcake):
-    size = "Regular"
-    
-    def __init__(self, name, price, flavor, frosting, filling):
-        self.name = name
-        self.price = price
-        self.flavor = flavor
-        self.frosting = frosting
-        self.sprinkles = []
-        self.filling = filling
-    
-        
+
+    def calculate_price(self, quantity):
+        return quantity * self.price
+
 class Large(Cupcake):
-    size = "Large"
-    
-    def __init__(self, name, price, flavor, frosting, filling):
-        self.name = name
-        self.price = price
-        self.flavor = flavor
-        self.frosting = frosting
-        self.sprinkles = []
-        self.filling = filling
-        
+    size = "large"
 
+    def calculate_price(self, quantity):
+        return quantity * self.price
 
-     
-cupcake1 = Regular("Stars and Stripes", 2.99, "Vanilla", "Chocolate", None)
+### Cupcake instances
+cupcake1 = Regular("Stars and Stripes", 2.99, "Vanilla", "Vanilla", "Chocolate")
 cupcake1.add_sprinkles("Red", "White", "Blue")
 cupcake2 = Mini("Oreo", .99, "Chocolate", "Cookies and Cream")
 cupcake2.add_sprinkles("Oreo pieces")
 cupcake3 = Large("Red Velvet", 3.99, "Red Velvet", "Cream Cheese", None)
-cupcake3.add_sprinkles("White")
-cupcake4 = Large("Strawberries and Creme", 3.99, "Vanilla", "Strawberry", "Strawberry")
-cupcake4.add_sprinkles("Red", "White")
-cupcake5 = Mini("Peanut Butter Fudge", 1.99, "Chocolate", "Peanut Butter")
-cupcake6 = Regular("Orange Creme", 2.99, "Vanilla", "Orange", "Orange")
+cupcake4 = Regular("Triple Chocolate", 2.99, "Chocolate", "Chocolate", "Chocolate")
+cupcake1.add_sprinkles("Chocolate")
+cupcake5 = Regular("Strawberry", 2.99, "Stawberry", "Vanilla", None)
 
 cupcake_list = [
     cupcake1,
     cupcake2,
     cupcake3,
     cupcake4,
-    cupcake5,
-    cupcake6
+    cupcake5
 ]
 
 
 
+### Functions to add cupcake classes to file
 def add_cupcake(file, cupcake):
     with open(file, "a", newline="\n") as csvfile:
         fieldnames = ["size", "name", "price", "flavor", "frosting", "sprinkles", "filling"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        
+
         if hasattr(cupcake, "filling"):
             writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "filling": cupcake.filling, "sprinkles": cupcake.sprinkles})
         else:
-           writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "sprinkles": cupcake.sprinkles}) 
-        
+            writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "sprinkles": cupcake.sprinkles})
+
+
 def write_new_csv(file, cupcakes):
     with open(file, "w", newline="\n") as csvfile:
         fieldnames = ["size", "name", "price", "flavor", "frosting", "sprinkles", "filling"]
-        writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
-        
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
         writer.writeheader()
         
         for cupcake in cupcakes:
             if hasattr(cupcake, "filling"):
-               writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "sprinkles": cupcake.sprinkles, "filling": cupcake.filling})
+                writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "filling": cupcake.filling, "sprinkles": cupcake.sprinkles})
             else:
-                 writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "sprinkles": cupcake.sprinkles})
-# write_new_csv("sample.csv", cupcake_list)
+                writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "sprinkles": cupcake.sprinkles})
 
+
+
+### Functions to add the cupcake dictionaries to file
 def get_cupcakes(file):
     with open(file) as csvfile:
         reader = csv.DictReader(csvfile)
@@ -115,19 +106,17 @@ def find_cupcake(file, name):
     for cupcake in get_cupcakes(file):
         if cupcake["name"] == name:
             return cupcake
-        
     return None
-        
+
 def add_cupcake_dictionary(file, cupcake):
     with open(file, "a", newline="\n") as csvfile:
         fieldnames = ["size", "name", "price", "flavor", "frosting", "sprinkles", "filling"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writerow(cupcake)
-        
+
 def read_csv(file):
     with open(file) as csvfile:
         reader = csv.DictReader(csvfile)
-        
+
         for row in reader:
             pprint(row)
-# read_csv("sample.csv")
--- starter/server.py
+++ starter/server.py
@@ -1,15 +1,54 @@
-from flask import Flask
+import csv
+from flask import Flask, render_template, url_for, redirect
+from cupcakes import get_cupcakes, find_cupcake, add_cupcake_dictionary
 
 app = Flask(__name__)
 
+
+
 @app.route("/")
 def home():
-    pass
+    cupcakes = get_cupcakes("sample.csv")
+    order = get_cupcakes("order.csv")
+    order_total = round(sum([float(x["price"]) for x in order])),
+    return render_template("index.html", cupcakes=cupcakes, items_num=len(order), order_total=order_total)
 
+@app.route("/all")
+def all():
+    return render_template("all.html")
 
+@app.route("/add/<name>")
+def add(name):
+    cupcake = find_cupcake("sample.csv", name)
+   
+    if cupcake:
+        add_cupcake_dictionary("order.csv", cupcake=cupcake)
+        return redirect (url_for("home"))
+    else:
+        return "Sorry, we do not have that!"
 
+@app.route("/each/<name>")
+def each(name):
+    cupcake=find_cupcake("sample.csv", name)
+    print(cupcake)
+    
+    if cupcake:
+        return render_template("each.html", cupcake=cupcake)
+    else:
+        return "Sorry cupcake not found."
 
-
+@app.route("/cart")
+def cart():
+    cupcakes=get_cupcakes("order.csv")
+    
+    cupcakes_counted = []
+    cupcake_set = set()
+    
+    for cupcake in cupcakes:
+        cupcake_set.add((cupcake["name"], cupcake["price"], cupcakes.count(cupcake)))
+        
+    return render_template("cart.html", cupcakes=cupcake_set)
+    
 
 
 
